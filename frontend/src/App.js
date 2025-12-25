3ef379d7629136a0d6a94d7def30bfe45fb41e9b
wjacob416-arch <wjacob416@gmail.com>
2025-12-25 11:52:32 -0600

    First frontend update w/books

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, { use } from "react";
+import { useState, useEffect } from "react";
+
 
 function App() {
-  const [books, setBooks] = useState([]);   // books starts as an empty list
+  const [books, setBooks] = useState([]);
   useEffect(() => {
     fetch("http://127.0.0.1:5000/books")
-      .then((response) => response.json())  // convert response to JSON
-      .then((data) => {
-        setBooks(data);                     // put JSON into books state
-      });
-  }, []); // [] means run only once, when page loads
+      .then((response) => response.json())
+      .then((books) => setBooks(books));
+  }, []);
+  function AddBooks() {
+    
+  }
   return (
     <div>
-      <h1>Book Tracker App</h1>
+      <h1>All Books</h1>
       <ul>
-        {/* 3. loop through books and show each title */}
         {books.map((book) => (
           <li key={book.id}>
-            {book.title} by {book.author} 
+            {book.title} by {book.author}
           </li>
         ))}
       </ul>
     </div>
   );
+}
+function LoadBooks(){
+
 }
 
 export default App;
