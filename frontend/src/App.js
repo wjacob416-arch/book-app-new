e8911aed210c203adfc732cf86a8544750ee0e30
wjacob416-arch <wjacob416@gmail.com>
2026-02-28 22:37:49 -0600

    Frontend Start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,17 @@
-import React, { use } from "react";
 import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-
-function App() {
+function HomePage(){
   const [books, setBooks] = useState([]);
   useEffect(() => {
     fetch("http://127.0.0.1:5000/books")
       .then((response) => response.json())
       .then((books) => setBooks(books));
   }, []);
-  function AddBooks() {
-    
-  }
-  return (
+   return (
     <div>
       <h1>All Books</h1>
+      <Link to="/add">Add a Book</Link>
       <ul>
         {books.map((book) => (
           <li key={book.id}>
@@ -25,8 +22,3 @@ function App() {
     </div>
   );
 }
-function LoadBooks(){
-
-}
-
-export default App;
